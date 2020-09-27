6e5db473d526c3b94268c4246c9ca3cf6dbb3dfe
Ryan Tharp <neuro@interx.net>
2020-09-26 22:56:01 -0700

    30s polling to 2min

--- loki-bridge/src/components/swapInfo/index.js
+++ loki-bridge/src/components/swapInfo/index.js
@@ -38,7 +38,7 @@ class SwapInfo extends PureComponent {
 
   componentDidMount() {
     // Run a timer every 10 seconds to refresh
-    this.timer = setInterval(this.props.onRefresh, 30 * 1000);
+    this.timer = setInterval(this.props.onRefresh, 2 * 60 * 1000);
 
     this.onResize();
     window.addEventListener('resize', this.onResize);
