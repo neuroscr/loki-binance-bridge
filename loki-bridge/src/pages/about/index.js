a085d2a52e55b04ab7854b37ac88455712980c74
Ryan Tharp <neuro@interx.net>
2020-09-29 03:13:12 +0000

    Headers cant go into p tgs

--- loki-bridge/src/pages/about/index.js
+++ loki-bridge/src/pages/about/index.js
@@ -18,7 +18,6 @@ class About extends Component {
     alignItems="center"
   >
     <Grid item md={8}>
-      <Typography>
 <p>Loki is a blockchain project that allows users to trade and communicate with increased levels of privacy. Loki runs on its own Monero-based blockchain, meaning Loki by itself can’t be used to interact with many popular Ethereum-based tools.</p>
 <p>In order to provide users greater access to Loki, Chainflip.io is providing a wrapping service with the support and oversight of the Loki Foundation.</p>
 <p>Wrapped Loki, or wLoki, is an ERC20-compatible token issued by the Loki Foundation on the Ethereum blockchain that can be exchanged for native Loki tokens on the Loki blockchain.</p>
@@ -29,7 +28,6 @@ class About extends Component {
 <p>When it’s time to turn your wLoki back into Loki tokens, you can unwrap wLoki by provably burning it through the bridge software. You must use a Web3.0 compatible wallet such as Metamask, which will unwrap your Loki by automatically including your Loki address in the burn transaction that you send through the wLoki bridge.</p>
 <h2>What's the future of wLoki?</h2>
 <p>wLoki is being provided as a service to the Loki and Chainflip community. This service will operate through Chainflip’s wLoki bridge until Chainflip launches its decentralised wLoki/Loki pool, at which point the bridge will be terminated in favour of this better solution. You can find out more about this decentralised method of cross-chain exchange by reading the Chainflip whitepaper, available at chainflip.io.</p>
-      </Typography>
     </Grid>
   </Grid>
 </div>
