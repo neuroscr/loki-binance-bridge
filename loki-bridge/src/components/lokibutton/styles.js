8b0013853895a4e3b63a23b2e68e0c359c331617
Ryan Tharp <neuro@interx.net>
2020-09-29 03:13:30 +0000

    CF button styling

--- loki-bridge/src/components/lokibutton/styles.js
+++ loki-bridge/src/components/lokibutton/styles.js
@@ -2,17 +2,35 @@ const styles = theme => ({
   root: {
     position: 'relative',
   },
+  label: {
+    "font-size": '0.825rem',
+  },
   button: {
     minWidth: '100px',
-    fontWeight: 700,
+    //fontWeight: 700,
+    display: 'inline-block',
+    fontWeight: 400,
     border: `1px solid ${theme.palette.primary.main}`,
     transition: 'all 0.2s ease-in-out',
-    padding: '0.8rem 2.5rem',
-    letterSpacing: '0.03em',
-    fontSize: '0.8rem',
+    //padding: '0.8rem 2.5rem',
+    //letterSpacing: '0.03em',
+    //fontSize: '0.8rem',
+    color: 'black',
+    'font-family': '"roboto-black", sans-serif',
+    'font-size': '1.4rem',
+    'text-transform': 'uppercase',
+    'letter-spacing': '.3rem',
+    //height: '5.4rem',
+    //'line-height': '5.4rem',
+    //padding: '0 3rem',
+    margin: '0 .8rem 1.2rem 0',
+    backgroundColor: 'white',
     '&:hover': {
-      backgroundColor: theme.palette.primary.main,
-      color: 'white'
+      background: '-webkit-linear-gradient(45deg, #DB58C0, #47EABB)',
+      color: '#000000',
+      outline: '0',
+      //backgroundColor: theme.palette.primary.main,
+      //color: 'white'
     }
   },
   secondary: {
